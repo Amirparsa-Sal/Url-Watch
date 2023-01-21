be0137ab2c8d6ead218f62a045b5b2e85d78df1c
Amirparsa-Sal <Amirparsa.s751@gmail.com>
2023-01-21 03:51:42 +0330

    implement user model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
 # Create your models here.
+class UserManager(BaseUserManager):
+
+    def create_user(self, email, first_name, last_name, password):
+        if not email:
+            raise ValueError('Users must have an email address.')
+        if not password:
+            raise ValueError('Users must have a password.')
+
+        email = self.normalize_email(email)
+        user = self.model(email=email,first_name=first_name, last_name=last_name)
+        user.password = password
+        
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self,email, password, first_name=None, last_name=None):
+        user = self.create_user(email, first_name, last_name, password)
+        user.set_password(password)
+        user.is_active = True
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+        return user
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=255, unique=True)
+    first_name = models.CharField(max_length=255, null=True)
+    last_name = models.CharField(max_length=255, null=True)
+    is_active = models.BooleanField(default=False) #if the account is activated
+    is_staff = models.BooleanField(default=False) #if is admin
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
--- url_watch/settings.py
+++ url_watch/settings.py
@@ -36,7 +36,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'api',
-    'restframework'
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -129,3 +129,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'api.User'
