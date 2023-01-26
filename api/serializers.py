3d26fb3344f73a9c2b917bb44c66c4df53b9ca88
Amirparsa-Sal <Amirparsa.s751@gmail.com>
2023-01-26 18:50:19 +0330

    add serilizers for URL APIs

--- api/serializers.py
+++ api/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
+from api.models import Url
 
 User = get_user_model()
 
@@ -9,8 +10,16 @@ class UserRegisterSerializer(serializers.ModelSerializer):
         model = User
         fields = ('email', 'first_name', 'last_name', 'password')
 
-class UserLoginSerializer(serializers.ModelSerializer):
-    ''' Serializer for logging in a user'''
+class UrlSerializer(serializers.ModelSerializer):
+    ''' Serializer for adding urls'''
     class Meta:
-        model = User
-        fields = ('email', 'password')
+        model = Url
+        read_only_fields = ('id', 'created_at', 'updated_at', 'failed_times')
+        fields = read_only_fields + ('url', 'threshold')
+
+class UrlCompactSerializer(serializers.ModelSerializer):
+    ''' Serializer for showing a list of urls'''
+    class Meta:
+        model = Url
+        fields = ('id', 'url')
+        
