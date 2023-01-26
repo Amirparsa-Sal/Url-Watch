d0cb4ba9601d3d145616f3c5cb7d1beebae28241
Amirparsa-Sal <Amirparsa.s751@gmail.com>
2023-01-26 22:30:58 +0330

    done url monitoring

--- monitor/tasks.py
+++ monitor/tasks.py
@@ -6,10 +6,10 @@ from celery import shared_task
 def monitor_urls():
     # Get all urls
     urls = Url.objects.all()
-    failed = True
     for url in urls:
-        # Check if we can get a response from the url
+        failed = True
         result_code = 0
+        # Check if we can get a response from the url
         try:
             response = requests.head(url.url)
             if response.status_code // 100 != 2:
@@ -25,10 +25,12 @@ def monitor_urls():
             url.save()
             result_code = -1
         
+        # if the url is not failed do nothing
         if not failed:
             continue
         
-        if url.url.failed_times > url.threshold:
+        # if the url is failed and the failed times is greater than the threshold then create a warning
+        if url.failed_times > url.threshold:
             # if the failed times is greater than the threshold then create a warning with result_code 0
             Warning.objects.create(url=url, result_code=result_code)
         
