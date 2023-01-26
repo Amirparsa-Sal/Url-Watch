e18016bf9168d4f59b918274e32e2a4c145466c0
Amirparsa-Sal <Amirparsa.s751@gmail.com>
2023-01-26 21:50:56 +0330

    fix monitoring function error detection

--- monitor/tasks.py
+++ monitor/tasks.py
@@ -11,7 +11,7 @@ def monitor_urls():
         result_code = 0
         try:
             response = requests.head(url.url)
-            if response.status_code % 100 != 2:
+            if response.status_code // 100 != 2:
                 # if the response is not 2xx then increase the failed times
                 url.failed_times += 1
                 url.save()
@@ -22,7 +22,7 @@ def monitor_urls():
             url.save()
             result_code = -1
         
-        if url.failed_times > url.threshold:
+        if url.url.failed_times > url.threshold:
             # if the failed times is greater than the threshold then create a warning with result_code 0
             Warning.objects.create(url=url, result_code=result_code)
 
